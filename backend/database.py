ac6dabdef9917c00dbcdd0d68581acd6fcf2715c
AdrianChabrera <adrianchabrera@gmail.com>
2026-04-20 18:07:46 +0200

    initial commit

---
+++ backend/database.py
@@ -0,0 +1,15 @@
+import os
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URI = os.getenv("NEO4J_URI")
+USER = os.getenv("NEO4J_USER")
+PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
+
+def get_db():
+    with driver.session() as session:
+        yield session
